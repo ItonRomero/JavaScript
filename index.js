3556d9e01f0b779badae477f7305799048330c6a
ItonRomero <jaysonromero1218@gmail.com>
2022-06-01 12:10:56 +0800

    Added increment funcion throug Console log

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//document.getElementById("count-el").innerText = 5
+
+// let bonusPoints =  5
+// bonusPoints = bonusPoints + 1
+// console.log(bonusPoints)
+
+// initialize the count as 0
+// listen for clicks on the increment Button
+// increment the count variable when button is clicked
+// change the count-el in the HTML to reflect the new count
+
+
+let count = 0
+
+ function increment() {
+    count = count + 1
+    
+    console.log(count)
+ }
