283af3db2e4a91321f4c61249ac2d35ac648216a
ItonRomero <jaysonromero1218@gmail.com>
2022-06-02 12:03:48 +0800

    added Save button w/ jS function

--- index.js
+++ index.js
@@ -4,16 +4,41 @@
 // listen for clicks on the increment Button
 // increment the count variable when button is clicked
 // change the count-el in the HTML to reflect the new count
-
-let countel = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
 
  function increment() {
     count = count + 1
-    countel.innerText = count
+    countEl.innerText = count
 
  }
 
- function save(){
-    console.log(count)
- }
+
+  function save() {
+  let countStr = count + " - "
+   saveEl.innerText += countStr
+    console.log(countStr)
+
+}
+
+//  let name = 25
+//  let greeting = "Hi, my name is"
+//  let myGreetings = name + "," + greeting + "!"
+
+//  console.log(myGreetings)
+
+// let welcomeEl = document.getElementById("welcome-el")
+// let name = "Jayson, "
+// let greeting = "What's up "
+//  welcomeEl.innerText = welcomeEl.innerText+" " +  name + greeting
+//or
+//  welcomeEl.innerText += " " + name + greeting
+
+
+
+
+
+
+
+
