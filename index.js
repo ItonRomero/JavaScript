94fd7cf1ccb948f962555d3167fa6c34e7adac0b
ItonRomero <jaysonromero1218@gmail.com>
2022-06-10 11:54:41 +0800

    Added javascript function for Chrome Extension section

--- index.js
+++ index.js
@@ -134,10 +134,26 @@ function newCard(){
 // ----------------------------------------------------------------------------------
 // CHROMER EXTENSION SECTION
 let myLeads = []
-inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 
 inputBtn.addEventListener("click", function(){
-    console.log("clicked")
+   myLeads.push(inputEl.value)
+    inputEl.value = ""
+   renderLeads()
 })
  
+function renderLeads(){
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+   //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+   listItems += `
+               <li>
+                     <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+               </li>
+                `
+} 
+   ulEl.innerHTML = listItems
+}
